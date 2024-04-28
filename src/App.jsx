17c12c66342b50299bf2f45e07f2fe0274539b9a
Marwa Jawad <marwajawad@Marwas-MacBook-Pro.local>
2024-04-28 12:54:16 +0400

    adding loading spinner

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React, { Profiler } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Card from "./card";
 import movie_img from "./assets/movie.jpeg";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import Home from "./Home";
 import SingleMovie from "./singleMovie";
 
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import ErrorBoundary from "./ErrorBoundry";
 function onRenderCallback(
   id,
   phase,
@@ -29,12 +31,14 @@ function onRenderCallback(
 function App() {
   return (
     <Profiler id="MyComponent" onRender={onRenderCallback}>
-      <Router>
-        <Routes>
-          <Route exact path="/" Component={Home} />
-          <Route exact path="/movie/:id" Component={SingleMovie} />
-        </Routes>
-      </Router>
+      <ErrorBoundary>
+        <Router>
+          <Routes>
+            <Route exact path="/" Component={Home} />
+            <Route exact path="/movie/:id" Component={SingleMovie} />
+          </Routes>
+        </Router>
+      </ErrorBoundary>
     </Profiler>
   );
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,18 @@
 import React, { useState, useEffect } from "react";
 import Card from "./card";
+import useCounter from "./useCounter";
 export const Context = React.createContext();
+
 function Home() {
   const [data, setData] = useState([]);
+
+  // 1-step is to create loading state
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
+    // 2-step to set setLoading to true
+    setLoading(true);
+
     const options = {
       method: "GET",
     };
@@ -14,7 +23,7 @@ function Home() {
       .then((result) => result.json())
       .then((movies) => {
         setData(movies.results);
-
+        setLoading(false);
         console.log(movies.results);
       });
   }, []);
@@ -22,10 +31,14 @@ function Home() {
   return (
     <div>
       <div className="d-flex flex-wrap justify-content-center">
-        {console.log(data, "current movies")}
-        {data?.map((item, i) => (
-          <Card movie={item} />
-        ))}
+        {/* 3-step render the date according to loading state */}
+        {loading ? (
+          <div class="spinner-border m-4 text-info" role="status">
+            <span class="sr-only">.</span>
+          </div>
+        ) : (
+          data?.map((item, i) => <Card movie={item} />)
+        )}
       </div>
     </div>
   );
